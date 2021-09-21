56d816bb73df8966d4c8e77c0ff6a5963d532d64
Hichami Houssam <hichami.houssam@gmail.com>
2021-09-21 13:52:26 +0100

    setting up webpack and list of tasks

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import './style.css';
+
+const tasks = [
+  {
+    description : "do squats",
+    completed : false,
+    index : 1
+
+  },
+  {
+    description : "do pull ups",
+    completed : false,
+    index : 2
+
+  },
+  {
+    description : "do push ups",
+    completed : false,
+    index : 3
+
+  }
+
+]

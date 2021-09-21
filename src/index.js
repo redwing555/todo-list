6881351819235af1765a1a4e125bb31b9a37a28e
Hichami Houssam <hichami.houssam@gmail.com>
2021-09-21 17:57:25 +0100

    populating todo list elements using js and add styling to the page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './style.css';
+/* eslint-disable */
+import _ from 'lodash';
+/* eslint-enable */
+
+const list = document.querySelector('.list');
 
 const tasks = [
   {
@@ -21,3 +26,20 @@ const tasks = [
   },
 
 ];
+
+function loadDomList() {
+  tasks.forEach((task) => {
+    const taskInfo = document.createElement('li');
+    const update = document.createElement('a');
+
+    update.innerHTML = `<button id="${task.index}" class="remove" > <i class="fa fa-ellipsis-v" aria-hidden="true"></i> </button>`;
+    taskInfo.innerHTML = `<input type="checkbox" class="task-box"> 
+                          <span> ${task.description} </span>`;
+
+    taskInfo.classList.add('li');
+    taskInfo.appendChild(update);
+    list.appendChild(taskInfo);
+  });
+}
+
+window.addEventListener('load', loadDomList);

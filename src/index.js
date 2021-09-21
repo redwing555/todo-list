e5eacbb0a4d6e04af9d53f3d7a56648201ceda6b
Hichami Houssam <hichami.houssam@gmail.com>
2021-09-21 14:16:08 +0100

    adding appropriate linters for HTML/CSS/JS

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-
 import './style.css';
 
 const tasks = [
   {
-    description : "do squats",
-    completed : false,
-    index : 1
+    description: 'do squats',
+    completed: false,
+    index: 1,
 
   },
   {
-    description : "do pull ups",
-    completed : false,
-    index : 2
+    description: 'do pull ups',
+    completed: false,
+    index: 2,
 
   },
   {
-    description : "do push ups",
-    completed : false,
-    index : 3
+    description: 'do push ups',
+    completed: false,
+    index: 3,
 
-  }
+  },
 
-]
+];

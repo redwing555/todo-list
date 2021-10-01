64137c0c5f03f343bcc7b7c90437139180813f06
Hichami Houssam <hichami.houssam@gmail.com>
2021-10-01 13:40:33 +0100

    testing edit func

--- src/methods.test.js
+++ src/methods.test.js
@@ -3,7 +3,7 @@
  */
 
 import { AddTask, createTask } from './crud';
-import { checkedTasksEvent, deleteCompletedTask } from './methods';
+import { checkedTasksEvent, deleteCompletedTask, editTask } from './methods';
 import localStorageMock from './__mocks__/localstorage';
 
 describe('edit / completed task state / deletion after completed', () => {
@@ -47,5 +47,29 @@ describe('edit / completed task state / deletion after completed', () => {
     checkedTasksEvent(tasks, checkbox);
 
     expect(tasks[0].completed).toBe(true);
+  });
+
+  test('task correctly edited in local storage and DOM', () => {
+    const tasks = JSON.parse(localStorage.getItem('tasksList')) || [];
+    document.body.innerHTML = '<div>'
+    + '<input type="text" class="input-task" placeholder="add task to your list ...">'
+    + '  <ul class="list"></ul>'
+    + '</div>';
+
+    createTask(0, 'do push ups', true);
+    document.querySelector('.input-task').value = 'do push ups';
+    AddTask(tasks);
+
+    const listel = document.querySelectorAll('.list li');
+    [...listel][0].classList.add('li-task');
+    const description = [...[...listel][0].children][1];
+    const contentEditable = [...description.attributes][1];
+
+    contentEditable.value = true;
+    description.textContent = 'do something';
+
+    editTask(description, tasks, [...listel][0]);
+
+    expect(tasks[0].description).toBe('do something');
   });
 });
